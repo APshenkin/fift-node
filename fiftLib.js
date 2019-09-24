cfab1aef1834a633bf4b9a1f8d9642968fbd35a9
Andrey Pshenkin <a.pshenkin@rambler-co.ru>
2019-09-24 12:33:46 +0300

    Add windows support

--- fiftLib.js
+++ fiftLib.js
@@ -1,4 +1,5 @@
 const ffi = require('ffi');
+const path = require('path');
 const ArrayType = require('ref-array');
 
 const StringArray = ArrayType('CString');
@@ -7,7 +8,15 @@ const { platform } = process;
 let fiftlibLoc = null;
 
 if (platform === 'darwin') {
-  fiftlibLoc = `${__dirname}/lib/libfiftlib.dylib`;
+  fiftlibLoc = path.join(__dirname, '/lib/libfiftlib.dylib');
+} else if (platform === 'win32') {
+  fiftlibLoc = path.join(__dirname, '/lib/fiftlib.dll');
+
+  // add /lib to dll directory (for windows we need linked libraries)
+  const kernel32 = ffi.Library('kernel32', {
+    SetDllDirectoryA: ['bool', ['string']],
+  });
+  kernel32.SetDllDirectoryA(path.join(__dirname, '/lib'));
 } else {
   throw new Error('unsupported platform for libfift');
 }
