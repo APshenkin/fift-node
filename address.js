436fc4b173b112cf1280f63f41bcf52099641da2
Andrey Pshenkin <a.pshenkin@rambler-co.ru>
2019-09-27 15:14:42 +0300

    Update smart contract for latest version. Add address converter

---
+++ address.js
@@ -0,0 +1,57 @@
+const crc16 = require('crc16');
+const base64url = require('base64url');
+
+class Address {
+  constructor({ workchainId, publicKey }) {
+    this.workchainId = workchainId;
+    this.publicKey = publicKey;
+  }
+
+  static parseFromRaw(string) {
+    let [workchainId, publicKey] = string.split(':');
+    workchainId = parseInt(workchainId, 10);
+    publicKey = Buffer.from(publicKey, 'hex');
+    return new Address({ workchainId, publicKey });
+  }
+
+  static parseFromUserFriendly(string) {
+    const payload = base64url.toBuffer(string);
+
+    return new Address({ workchainId: payload.readInt8(1), publicKey: payload.toString('hex', 2, 34) });
+  }
+
+  static parseFromFift(string) {
+    const payload = Buffer.from(string, 'hex');
+
+    return new Address({ workchainId: payload.readInt32BE(32), publicKey: payload.toString('hex', 0, 32) });
+  }
+
+  toRaw() {
+    return `${this.workchainId}:${this.publicKey}`.toUpperCase();
+  }
+
+  toUserFriendly(nonBounce = false) {
+    const tag = nonBounce ? 0x51 : 0x11;
+
+    const payload = Buffer.alloc(34);
+
+    payload.fill(tag, 0, 1);
+    payload.writeInt8(this.workchainId, 1);
+    payload.fill(this.publicKey, 2);
+
+    const crc = crc16(payload);
+
+    return base64url(Buffer.concat([payload, Buffer.from(crc.toString(16), 'hex')]));
+  }
+
+  toFift() {
+    const payload = Buffer.alloc(36);
+
+    payload.fill(this.publicKey);
+    payload.writeInt32BE(this.workchainId, 32);
+
+    return payload.toString('hex').toUpperCase();
+  }
+}
+
+module.exports = Address;
---
+++ tests/address.test.js
@@ -0,0 +1,28 @@
+const assert = require('assert');
+const Address = require('../address');
+
+describe('address', () => {
+  it('should parse raw address and show it to UserFriendly', async () => {
+    const res = Address.parseFromRaw('-1:FCB91A3A3816D0F7B8C2C76108B8A9BC5A6B7A55BD79F8AB101C52DB29232260').toUserFriendly();
+
+    assert.equal(res, 'Ef_8uRo6OBbQ97jCx2EIuKm8Wmt6Vb15-KsQHFLbKSMiYDJ4');
+  });
+
+  it('should parse user friendly and show it to raw', async () => {
+    const res = Address.parseFromUserFriendly('Ef_8uRo6OBbQ97jCx2EIuKm8Wmt6Vb15-KsQHFLbKSMiYDJ4').toRaw();
+
+    assert.equal(res, '-1:FCB91A3A3816D0F7B8C2C76108B8A9BC5A6B7A55BD79F8AB101C52DB29232260');
+  });
+
+  it('should parse from fift hex file', async () => {
+    const res = Address.parseFromFift('FCB91A3A3816D0F7B8C2C76108B8A9BC5A6B7A55BD79F8AB101C52DB29232260FFFFFFFF').toRaw();
+
+    assert.equal(res, '-1:FCB91A3A3816D0F7B8C2C76108B8A9BC5A6B7A55BD79F8AB101C52DB29232260');
+  })
+
+  it('should parse from raw and show in fift', async () => {
+    const res = Address.parseFromRaw('-1:FCB91A3A3816D0F7B8C2C76108B8A9BC5A6B7A55BD79F8AB101C52DB29232260').toFift();
+
+    assert.equal(res, 'FCB91A3A3816D0F7B8C2C76108B8A9BC5A6B7A55BD79F8AB101C52DB29232260FFFFFFFF');
+  })
+});
