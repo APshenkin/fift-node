32fa64e607ca468343ff2cfb472ee2920f237758
Andrey Pshenkin <a.pshenkin@rambler-co.ru>
2019-10-03 17:38:53 +0300

    linked libs include. add linux build rename publickey to account in address

--- address.js
+++ address.js
@@ -2,30 +2,30 @@ const crc16 = require('crc16');
 const base64url = require('base64url');
 
 class Address {
-  constructor({ workchainId, publicKey, fift }) {
+  constructor({ workchainId, account, fift }) {
     this.workchainId = workchainId;
-    this.publicKey = publicKey;
+    this.account = account;
     this.fift = fift;
   }
 
   static parseFromRaw(string) {
     // eslint-disable-next-line prefer-const
-    let [workchainId, publicKey] = string.split(':');
+    let [workchainId, account] = string.split(':');
     workchainId = parseInt(workchainId, 10);
-    publicKey = Buffer.from(publicKey, 'hex');
-    return new Address({ workchainId, publicKey });
+    account = Buffer.from(account, 'hex');
+    return new Address({ workchainId, account });
   }
 
   static parseFromUserFriendly(string) {
     const payload = base64url.toBuffer(string);
 
-    return new Address({ workchainId: payload.readInt8(1), publicKey: payload.slice(2, 34) });
+    return new Address({ workchainId: payload.readInt8(1), account: payload.slice(2, 34) });
   }
 
   static parseFromFift(string) {
     const payload = Buffer.from(string, 'hex');
 
-    return new Address({ workchainId: payload.readInt32BE(32), publicKey: payload.slice(0, 32) });
+    return new Address({ workchainId: payload.readInt32BE(32), account: payload.slice(0, 32) });
   }
 
   static async parseFromWalletPrivateKey({ privateKey, workchainId, fift }) {
@@ -34,7 +34,7 @@ class Address {
   }
 
   toRaw() {
-    return `${this.workchainId}:${this.publicKey.toString('hex')}`.toUpperCase();
+    return `${this.workchainId}:${this.account.toString('hex')}`.toUpperCase();
   }
 
   toUserFriendly({ nonBounce = false, mainnet = false } = {}) {
@@ -44,7 +44,7 @@ class Address {
 
     payload.fill(tag, 0, 1);
     payload.writeInt8(this.workchainId, 1);
-    payload.fill(this.publicKey, 2);
+    payload.fill(this.account, 2);
 
     const crc = crc16(payload);
 
@@ -54,7 +54,7 @@ class Address {
   toFift() {
     const payload = Buffer.alloc(36);
 
-    payload.fill(this.publicKey);
+    payload.fill(this.account);
     payload.writeInt32BE(this.workchainId, 32);
 
     return payload.toString('hex').toUpperCase();
